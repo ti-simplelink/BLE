d6c4ad990c440a1438521f9e934477b98c8ddb57
Sean Lyons <a0225155@ti.com>
2016-10-31 11:27:44 -0700

    Add support for bluefruit controller
    See BLE_ENERGIA-1

--- BLE/examples/SerialOverBLE/SerialOverBLE.ino
+++ BLE/examples/SerialOverBLE/SerialOverBLE.ino
@@ -1,18 +1,66 @@
 
 #include <BLE.h>
+#include <String.h>
+
+/* Pin number variables for LEDs on MSP432P401R LaunchPad */
+uint8_t led1Pin = 78;
+uint8_t led2RedPin = 75;
+uint8_t led2GreenPin = 76;
+uint8_t led2BluePin = 77;
+
+/* +1 for null-terminator */
+int numBytes = 0;
+char serialData[BLE_SERIAL_BUFFER_SIZE + 1];
+
 
 void setup() {
   Serial.begin(115200);
   ble.setLogLevel(BLE_LOG_ERRORS);
+  // Initalize I/O
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2RedPin, OUTPUT);
+  pinMode(led2GreenPin, OUTPUT);
+  pinMode(led2BluePin, OUTPUT);
   ble.begin();
   ble.serial();
   ble.setAdvertName("Energia Serial");
   ble.startAdvert();
 }
 
-/* +1 for null-terminator */
-int numBytes = 0;
-char serialData[BLE_SERIAL_BUFFER_SIZE + 1];
+void parseSerialString(String dataString)
+{
+  // Check if we found Adafruit app msg start char
+  if('!' == dataString.charAt(0))
+  {
+    switch (dataString.charAt(1))
+    {
+      case 'C':
+      {
+        // RGB color packet is of the form "[!],[C], [R byte], [G Byte], [B byte]"
+        Serial.println("Received Adafruit RGB LED Color Packet");
+        char redVal = dataString.charAt(2);
+        char greenVal = dataString.charAt(3);
+        char blueValue = dataString.charAt(4);
+
+        // Use PWM to write the color to the RGB LED
+        analogWrite(led2RedPin, redVal);
+        analogWrite(led2GreenPin, greenVal);
+        analogWrite(led2BluePin, blueValue);
+        break;
+      }
+      case 'B':
+      {
+        // Buttons packet is of the form: "[!], [B], [Button #] [1 (pressed)/0 (released)]
+        String state = (('1' == dataString.charAt(3)) ? " Pressed" : "  Released");
+        String result = "Received Adafruit Button Packet: Button ";
+        result += dataString.charAt(2) + state;
+        Serial.println(result);
+      }
+      default:
+        break;
+    }
+  }
+}
 
 void loop() {
   ble.handleEvents();
@@ -31,6 +79,7 @@ void loop() {
   if (ble.available())
   {
     /* All the Serial functions are available with BLE serial. */
-    Serial.println(ble.readString());
+    String leData = ble.readString();
+    parseSerialString(leData);
   }
 }
